cd5a14a650d96d2adbf6d79f11cef2d054f89a8e
catnipped <ossianboren@gmail.com>
2018-01-23 14:36:14 +0100

    thumbnail > fullview

--- scripts.js
+++ scripts.js
@@ -1,14 +1,25 @@
 var jsonplaceholder = 'https://jsonplaceholder.typicode.com/photos'
 var albumId = 1
 var fullImage = ''
-var fullImageId = 0
 
-document.onload = function() {
-    console.log('start fetching');
+function nextAlbum() {
+    albumId += 1;
+    if (albumId > 100) {
+        albumId = 1;
+    }
+    fetchFullAlbum(albumId);
+};
+
+function previousAlbum() {
+    albumId -= 1;
+    if (albumId < 1) {
+        albumId = 100;
+    };
     fetchFullAlbum(albumId);
 };
 
 function fetchFullAlbum(albumId) {
+    $('#thumbnails').empty();
     $.ajax({
         url: jsonplaceholder,
         type: 'GET',
@@ -28,5 +39,27 @@ function fetchFullAlbum(albumId) {
 };
 
 function addThumbnail(image) {
-  $('#thumbnails').append('<div class="thumbnail"><span>' + image.title + '</span><img src="' + image.thumbnailUrl + '"/></div>' )
+  $('#thumbnails').append('<div class="thumbnail" onclick="showFullImage(\'' + image.url + '\', \'' + image.title + '\')"><span>' + image.title + '</span><img src="' + image.thumbnailUrl + '"/></div>' )
 };
+
+function showFullImage(imageUrl, imageTitle) {
+    console.log(imageUrl + ' ' + imageTitle)
+    $('#fullImage').append('<span>' + imageTitle + '</span><img src="' + imageUrl + '"/>');
+}
+
+function hideFullImage() {
+    $('#fullImage').empty();
+}
+
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
